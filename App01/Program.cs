a821d2b8a878ff256fee473ccf33ae5ff006fbc5
Saragov Dumitru <saragov.dumitru@elev.cihcahul.md>
2022-03-02 15:01:42 +0200

    Am creat ramura suma

--- App01/Program.cs
+++ App01/Program.cs
@@ -6,7 +6,13 @@ namespace App01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Dati numarul a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dati numarul b:");
+            int b = int.Parse(Console.ReadLine());
+            int rezultat; rezultat = a + b;
+            Console.WriteLine("Rezultatul este: {0}", rezultat);
+            Console.ReadKey();
         }
     }
 }
